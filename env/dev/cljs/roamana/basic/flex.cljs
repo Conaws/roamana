335bd8ddf40df569d6ba0e810c277aba13220edb
Conor White-Sullivan <cwhitesullivan@gmail.com>
2016-08-16 11:26:11 +0530

    removed paths

--- env/dev/cljs/roamana/basic/flex.cljs
+++ env/dev/cljs/roamana/basic/flex.cljs
@@ -38,16 +38,6 @@
 
 
 
-(declarepath sclean)
-(providepath sclean
-             (if-path
-              coll?
-              (stay-then-continue
-               [ALL sclean])
-              ))
-
-
-
 (defn clean-top  [v]
   (transform ALL
              (fn [x]
@@ -64,17 +54,6 @@
 
 
 
-(comment 
-
-  (select [sclean (walker vector?)] [1 2 [3 4] 5 6])
-
-  (transform [sclean
-              (walker vector?)]
-             (fn [s]
-               (if (< 1 (count s))
-                 s
-                 ["12"] ))
-             [1 [2 [4] 3] [3]]))
 
 
 (declare render-frame*
